c589009d219e75debc588552dd5c05b09569e4bf
Nisarg Patel <nisargp@umich.edu>
2019-01-14 10:13:47 -0500

    Try

---
+++ Intro/Intro/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            Console.WriteLine("Hello World x2");
+
+            int a, b, c, d, e, f;
+            a = b = c = d = e = f = 10;
+            Console.WriteLine(e);
+        }
+    }
+}
